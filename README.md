c8254979f3a2363abe975664c8aef47ba0e7d5a2
Milking2k <Milking2k@qq.com>
2019-03-09 21:48:15 +0800

    重新整理格式；添加http编码；修改l日志记录模块

--- README.md
+++ README.md
@@ -8,3 +8,9 @@ a personal server with c code.
 - xinetd
 - download the file to your linux
 - change the /etc/xinetd.d
+
+
+
+### future:
+
+I will let the myhttpd.c more function to finish a service should do. And the xinetd I will change it be myself way to complete it's mission.
--- myhttpd.c
+++ myhttpd.c
@@ -3,6 +3,7 @@
 #include<stdlib.h>
 #include<string.h>
 #include<time.h>
+#include<iconv.h>
 #include<ctype.h>
 const int N = 2096;
 
@@ -12,15 +13,16 @@ const int N = 2096;
 //         fflush(tmp);\
 //     } while (0)
 struct tm *getNowTime();
-void getlog(const char *info, const char *str);
-void send_header(const int status, 
-                char const *type, 
-                char const *file_type, 
-                char const *protocol);
-void send_file(char *file);
-void send_error(const int status, char const *info);
-void checkHeader(const int argc, char const *argv[]);
+static void send_header(const char *protocol, const int status, const char *type, const char *file_type, const char *msg);
+static void send_error(const int status, char const *title, char *text);
+static void strencode(const char *from, char *to, size_t tosize);
+static void strdecode(const char *from, char *to, size_t tosize);
+static void getlog(const char *info, const char *more);
+static char *get_mine_type(const char *type);
+static void send_file(char *file);
 
+static void check(const int argc, char const *argv[]);
+static char *strupr(char *str); // change the string to lower;
 
 int main(int argc, char const *argv[])
 {
@@ -31,22 +33,22 @@ int main(int argc, char const *argv[])
 
     check(argc, argv);
     if(fgets(line, N, stdin) == NULL)
-        send_error(404, "file doesn't exsist!");
+        send_error(400, "Bad Request", "Header error");
     if(sscanf(line, "%[^ ] %[^ ] %[^ ]", method, path, protocol) != 3)
-        send_error(400, "fenjie error");
+        send_error(400, "Bad Request", "Release header error");
     while(fgets(line, N, stdin) != NULL)
         if(strcmp(line, "\r\n"))
             break;
 
-
+    getlog(method, path);
     // choose method
-    if(stricmp(method, "GET") != 0)
-        send_error(400, "method error");
+    if(strcmp(strupr(method), "GET") != 0)
+        send_error(400, "Bad Request", "Not GET method");
 
 
     // get file name
     if(path[0] != '/')
-        send_error(400, "path error");
+        send_error(400,"Bad Request" ,"Path error");
     file = path + 1;
     
     if(*file == '\0') send_file("index.html");
@@ -55,74 +57,124 @@ int main(int argc, char const *argv[])
     }
 
     fflush(stdout);
+
+    getlog("finished once http get","");
     return 0;
 }
 
 
-struct tm *getNowTime()
+// struct tm *getNowTime()
+// {
+//     time_t timep;
+//     time(&timep);
+//     struct tm *p = localtime(&timep);
+//     return p;
+// }
+void getlog(const char *info,const char *more)
 {
-    time_t timep;
-    time(&timep);
-    struct tm *p = localtime(&timep);
-    return p;
-}
-void getlog(const char *info,const char *str)
-{
-    FILE *tmp = fopen("log.txt", "a");
+    FILE *tmp = fopen("../logs/mailError.txt", "a");
     if(tmp == NULL) {
         perror("fopen tmp.txt error");
         exit(1);
     }
-    
-    fprintf(tmp, "%d/%d/%d-%d:%d:%d: ", 
-        getNowTime()->tm_year + 1900,
-        getNowTime()->tm_mon,
-        getNowTime()->tm_mday,
-        getNowTime()->tm_hour,
-        getNowTime()->tm_min,
-        getNowTime()->tm_sec);
+
+    time_t timep;
+    time(&timep);
+    struct tm *p = localtime(&timep);
+
+    fprintf(tmp, "%d/%d/%d-%02d:%02d:%02d: ", 
+        p->tm_year + 1900,
+        p->tm_mon,
+        p->tm_mday,
+        p->tm_hour,
+        p->tm_min,
+        p->tm_sec);
     do {
-        fprintf(tmp, "%s%s\n", info, str);
+        fprintf(tmp, "%s%s\n", info, more);
         fflush(tmp);
     } while (0);
     fclose(tmp);
 }
 
-void send_error(const int status, char const *info)
+void send_error(const int status, char const *title, char *text)
 {
-    printf("HTTP/1.1 %d ERROR\n", status);
+    printf("HTTP/1.1 %d %s\n", status, title);
     printf("content-Type: text/html\n\n");
+    printf("<h1>%d %s: %s</h1>",status, title, text);
 
-    printf("%d %s",status, info);
+    // sprintf(text, "error<%d>:%s", status, title);
+
+    getlog("error:", " ");
     exit(1);
 }
 
-void send_header(const char *protocol,
-                const int status, 
-                const char *type, 
-                const char *file_type, 
-                const char *msg)
+void send_header(const char *protocol, const int status, const char *type, const char *file_type, const char *msg)
 {
     printf("%s %d %s\n", protocol, status, type);
     printf("content-Type: %s;charset=utf8\n\n", file_type);
-}
-void check(const int argc, char const *argv[])
-{
-    if(argc != 2)
-        send_error(500, "no input");
-    if(chdir(argv[1]) == -1)
-        send_error(500, "service error");
+
 }
 
 void send_file(char *file)
 {
     int ich;
     FILE *fp = fopen(file, "r");
-    if(fp == NULL) send_error(404, file);
+    if(fp == NULL) send_error(404,"File not found","File not exsist");
 
     printf("HTTP/1.1 200 ok\n");
-    printf("content-Type: text/plain;charset=utf8\n\n");
+    // printf("content-Type: text/%s\n\n", get_mine_type(file));
+    printf("content-Type: text/html;charset=utf8\n\n");
     while((ich = getc(fp)) != EOF)
         putchar(ich);
     fclose(fp);
+}
+void check(const int argc, char const *argv[])
+{
+    if(argc != 2)
+        send_error(500, "Service Error", "Path didn't input");
+    if(chdir(argv[1]) == -1)
+        send_error(500, "Service Error", "Not a correct path");
+}
+
+char *strupr(char *str)
+{
+    int len = strlen(str);
+    for (int i = 0; i < len; ++i) 
+        if (islower(str[i]))
+            str[i] = toupper(str[i]);
+
+    return str;
+}
+
+
+char *get_mine_type(const char *type)
+{
+    if (strcmp(type, "html") == 0) {
+        return "text/html;charset=utf8;";
+    }
+    if (strcmp(type, "css") == 0) {
+        return "text/css;charset=utf8;";
+    }
+    if (strcmp(type, "jpg") == 0) {
+        return "image/jpg;charset=utf8;";
+    }
+    if (strcmp(type, "png") == 0) {
+        return "image/png;charset=utf8;";
+    }
+    
+
+    return "text/plain;charset=utf8;";
+}
+
+static void strencode(const char *from, char *to, size_t tosize)
+{
+    iconv_t cd=iconv_open("UTF-8", "UNICODE");
+    char *outbuf = (char *) malloc(tosize * 4);
+    iconv(cd, &from, (size_t*)&tosize, &to, &tosize);
+    free(outbuf);
+    iconv_close(cd);
+}
+static void strdecode(const char *from, char *to, size_t tosize)
+{
+
 }
